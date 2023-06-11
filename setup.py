4fe0c08039db86dba3f1a7e459b49fa52e9c2597
Ashu Singh <ishu.111636@gmail.com>
2023-06-11 23:32:05 +0530

    version method upgraded

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
+from djangolazy.__init__ import __version__
 with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
 setup(
   name = 'djangolazy',         # How you named your package folder (MyLib)
   packages = ['djangolazy'],   # Chose the same as "name"
-  version = '0.3.0',      # Start with a small number and increase it with every change you make
+  version = f'{__version__}',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Script for initial django project setup',   # Give a short description about your library
   long_description = long_description,
@@ -13,7 +14,7 @@ setup(
   author = ' Ishu Singh',                   # Type in your name
   author_email = 'ishu.111636@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/IshuSinghSE',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/IshuSinghSE/djangolazy/archive/refs/tags/v.0.1.0.tar.gz',    # I explain this later on
+  download_url = f'https://github.com/IshuSinghSE/djangolazy/archive/refs/tags/v.{__version__}.tar.gz',    # I explain this later on
   keywords = ['django', 'setup', 'autosetup','setupscript'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
             'urllib3',
