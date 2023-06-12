2ecde5e9e80bc6c1d9e86662538580e6aa0a4ccc
Ashu Singh <ishu.111636@gmail.com>
2023-06-12 20:05:23 +0530

    version added

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from distutils.core import setup
-from djangolazy.__init__ import __version__
+from setuptools import setup
+from djangolazy import __version__ as version
+
 with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
 setup(
   name = 'djangolazy',         # How you named your package folder (MyLib)
   packages = ['djangolazy'],   # Chose the same as "name"
-  version = f'{__version__}',      # Start with a small number and increase it with every change you make
+  version = version,      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Script for initial django project setup',   # Give a short description about your library
   long_description = long_description,
@@ -14,17 +15,27 @@ setup(
   author = ' Ishu Singh',                   # Type in your name
   author_email = 'ishu.111636@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/IshuSinghSE',   # Provide either the link to your github or to your website
-  download_url = f'https://github.com/IshuSinghSE/djangolazy/archive/refs/tags/v.{__version__}.tar.gz',    # I explain this later on
+  download_url = f'https://github.com/IshuSinghSE/djangolazy/archive/refs/tags/v.{version}.tar.gz',    # I explain this later on
   keywords = ['django', 'setup', 'autosetup','setupscript'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  python_requires= '>=3.7',
+  entry_points='djangolazy = djangolazy:version',
+  install_requires=[
             'urllib3',
             ],
   classifiers=[
-    'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
+    'Natural Language :: English',
+    'Operating System :: Microsoft :: Windows',
+    'Programming Language :: Python',
     'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3 :: Only',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
     'Programming Language :: Python :: 3.11',
   ],
+  include_package_data=True,
+      zip_safe=False
 )
